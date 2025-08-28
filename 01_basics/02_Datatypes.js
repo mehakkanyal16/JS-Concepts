c4fcec30d3db9f4e87613982144c109bde3ce61c
Mehak kanyal <149966478+mehakkanyal16@users.noreply.github.com>
2025-08-28 10:29:46 +0000

    docs: add notes on JavaScript data types and ECMAScript versions

---
+++ 01_basics/02_Datatypes.js
@@ -0,0 +1,22 @@
+"use strict"
+let a = 10;       // number
+let b = "12";     // string
+let c = true;     // boolean
+let state;        // undefined (since no value is assigned)
+
+// JS Data Types  - primitive, non-primitive
+
+//primitive =>number,bigint,boolean,string,null,undefined,symbol
+//non-primitive =>object,array,function
+
+// number   → 10, 3.14
+// bigint   → 12345678901234567890n
+// boolean  → true / false
+// string   → "text"
+// null     → standalone value (intentional absence of object) bug in JS (legacy reason).
+// undefined→ default value of uninitialized variables
+// symbol   → unique identifier
+
+console.log(typeof undefined) // undefined 
+console.log(typeof null) //object
+
