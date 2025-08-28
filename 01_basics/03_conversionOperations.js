dc79c91ab6c5f29d2d7d90e68535fe178af1af96
Mehak kanyal <149966478+mehakkanyal16@users.noreply.github.com>
2025-08-28 11:18:23 +0000

    docs: add examples of type conversion and operations in JavaScript

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,89 @@
+let score = "hitesh"
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+let isLoggedIn = "hitesh"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//  console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+
+// 🔹 Type Conversion
+// String → Number
+// Number("33")     // 33
+// Number("33abc")  // NaN (typeof = "number")
+// Number(true)     // 1
+// Number(false)    // 0
+// Number(null)     // 0
+// Number(undefined)// NaN
+
+// Any → Boolean
+// Boolean(1)        // true
+// Boolean(0)        // false
+// Boolean("")       // false
+// Boolean("hitesh") // true
+// Boolean(null)     // false
+
+// Number → String
+// String(33)     // "33"
+// String(true)   // "true"
+// String(null)   // "null"
+
+
+// *********************** Operations ***********************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " hitesh"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2); // 12
+// console.log(1 + "2");  //12
+// console.log("1" + 2 + 2); //122
+// console.log(1 + 2 + "2"); // 32
+
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
